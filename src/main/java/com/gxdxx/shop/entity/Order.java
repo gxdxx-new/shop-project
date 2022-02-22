51840e68e53f9553d4efc81244c2c3225868bee9
gxdxx <nkd0310@naver.com>
2022-02-22 15:10:27 +0900

    feat: 주문 엔티티 설계

---
+++ src/main/java/com/gxdxx/shop/entity/Order.java
@@ -0,0 +1,37 @@
+package com.gxdxx.shop.entity;
+
+import com.gxdxx.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime registerTime;
+
+    private LocalDateTime updateTime;
+
+}
