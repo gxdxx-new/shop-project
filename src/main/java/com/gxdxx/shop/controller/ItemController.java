118e9607be8b39c40bdd51a7d8ae43d34335da96
gxdxx <nkd0310@naver.com>
2022-02-24 16:12:23 +0900

    feat: 빈 상품 수정 페이지 수정

--- src/main/java/com/gxdxx/shop/controller/ItemController.java
+++ src/main/java/com/gxdxx/shop/controller/ItemController.java
@@ -1,13 +1,21 @@
 package com.gxdxx.shop.controller;
 
+import com.gxdxx.shop.dto.ItemFormDto;
+import com.gxdxx.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
 
+    private final ItemService itemService;
+
     @GetMapping(value = "/admin/item/new")
-    public String itemForm() {
+    public String itemForm(Model model) {
+        model.addAttribute("itemFormDto", new ItemFormDto());
         return "item/itemForm";
     }
 
