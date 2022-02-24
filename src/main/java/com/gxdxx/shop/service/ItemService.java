2dffea1d8a9f1a8fc5f00e47017b20d831052e45
gxdxx <nkd0310@naver.com>
2022-02-24 20:58:37 +0900

    feat: 상품 등록 서비스 클래스 생성

---
+++ src/main/java/com/gxdxx/shop/service/ItemService.java
@@ -0,0 +1,45 @@
+package com.gxdxx.shop.service;
+
+import com.gxdxx.shop.dto.ItemFormDto;
+import com.gxdxx.shop.entity.Item;
+import com.gxdxx.shop.entity.ItemImg;
+import com.gxdxx.shop.repository.ItemImgRepository;
+import com.gxdxx.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+    private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+
+        //상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지 등록
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+            if (i == 0) {
+                itemImg.setRepImgYn("Y");
+            } else {
+                itemImg.setRepImgYn("N");
+            }
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+
+}
