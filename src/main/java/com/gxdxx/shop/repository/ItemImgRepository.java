9c61dfc9785622b1899e60a8ccbec1205f8b8b46
gxdxx <nkd0310@naver.com>
2022-02-24 20:43:06 +0900

    feat: 상품 이미지 리포지터리 생성

---
+++ src/main/java/com/gxdxx/shop/repository/ItemImgRepository.java
@@ -0,0 +1,8 @@
+package com.gxdxx.shop.repository;
+
+import com.gxdxx.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+}
