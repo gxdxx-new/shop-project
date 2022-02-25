335d20b5cc8823b94c45ff800388f38239dd9808
gxdxx <nkd0310@naver.com>
2022-02-25 15:55:11 +0900

    feat: 등록된 상품 불러오는 메소드 상품 서비스 클래스에 추가

--- src/main/java/com/gxdxx/shop/service/ItemService.java
+++ src/main/java/com/gxdxx/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.gxdxx.shop.service;
 
 import com.gxdxx.shop.dto.ItemFormDto;
+import com.gxdxx.shop.dto.ItemImgDto;
 import com.gxdxx.shop.entity.Item;
 import com.gxdxx.shop.entity.ItemImg;
 import com.gxdxx.shop.repository.ItemImgRepository;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -40,6 +43,23 @@ public class ItemService {
         }
 
         return item.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId) {
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+
+        return itemFormDto;
     }
 
 }
