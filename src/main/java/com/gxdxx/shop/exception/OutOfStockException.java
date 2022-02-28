d9b7d3183029e4a8e344f181a5fe41b7661611b7
gxdxx <nkd0310@naver.com>
2022-02-28 15:13:07 +0900

    feat: 상품의 주문 수량보다 재고가 적을 때 발생하는 예외 정의

---
+++ src/main/java/com/gxdxx/shop/exception/OutOfStockException.java
@@ -0,0 +1,9 @@
+package com.gxdxx.shop.exception;
+
+public class OutOfStockException extends RuntimeException {
+
+    public OutOfStockException(String message) {
+        super(message);
+    }
+
+}
