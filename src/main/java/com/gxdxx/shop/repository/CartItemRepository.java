a99f6b80f13b380e5121b7dc9fcbdfccfb9e449a
gxdxx <nkd0310@naver.com>
2022-03-02 17:20:31 +0900

    feat: 장바구니에 담긴 상품 찾는 쿼리 추가

---
+++ src/main/java/com/gxdxx/shop/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package com.gxdxx.shop.repository;
+
+import com.gxdxx.shop.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+}
