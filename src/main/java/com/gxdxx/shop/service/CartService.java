7450af10fc565f2b3af6aff3871d5fb46e50d11d
gxdxx <nkd0310@naver.com>
2022-03-02 17:21:27 +0900

    feat: 장바구니에 상품 담는 로직 서비스 클래스에 구현

---
+++ src/main/java/com/gxdxx/shop/service/CartService.java
@@ -0,0 +1,49 @@
+package com.gxdxx.shop.service;
+
+import com.gxdxx.shop.dto.CartItemDto;
+import com.gxdxx.shop.entity.Cart;
+import com.gxdxx.shop.entity.CartItem;
+import com.gxdxx.shop.entity.Item;
+import com.gxdxx.shop.entity.Member;
+import com.gxdxx.shop.repository.CartItemRepository;
+import com.gxdxx.shop.repository.CartRepository;
+import com.gxdxx.shop.repository.ItemRepository;
+import com.gxdxx.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private CartItemRepository cartItemRepository;
+
+    public Long addCart(CartItemDto cartItemDto,String email) {
+        Item item = itemRepository.findById(cartItemDto.getItemId()).orElseThrow(EntityNotFoundException::new);
+        Member member = memberRepository.findByEmail(email);
+
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if (cart == null) {
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+
+        CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
+
+        if (savedCartItem != null) {
+            savedCartItem.addCount(cartItemDto.getCount());
+            return savedCartItem.getId();
+        }
+        CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
+        cartItemRepository.save(cartItem);
+        return cartItem.getId();
+    }
+
+}
